b10028be76fc31f3306aeb0965d18c14ee381603
simao <gabrielsimao@hotmail.com>
2020-08-26 14:20:55 -0300

    Iniciando desenvolvimento da pagina turmas.html

--- src/main/java/com/conquer/cursos/controller/MainController.java
+++ src/main/java/com/conquer/cursos/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.conquer.cursos.controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -12,5 +10,4 @@ public class MainController {
     public String main() {
         return "home"; //view
    }
-
 }
--- src/main/java/com/conquer/cursos/controller/TurmaController.java
+++ src/main/java/com/conquer/cursos/controller/TurmaController.java
@@ -1,25 +1,29 @@
 package com.conquer.cursos.controller;
 
 
-import com.conquer.cursos.DTO.AlunoDTO;
 import com.conquer.cursos.DTO.TurmaDTO;
-import com.conquer.cursos.model.entity.Aluno;
 import com.conquer.cursos.model.entity.Turma;
 import com.conquer.cursos.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 
-@RestController
+@Controller
 @RequestMapping("/turmas")
 public class TurmaController {
 
     @Autowired
     private TurmaService service;
+
+    @GetMapping
+    public String pageTurmas(){
+        return "turmas";
+    }
 
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody TurmaDTO dto){

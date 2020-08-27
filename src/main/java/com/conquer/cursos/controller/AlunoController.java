cfb1d5c833c75a8b412cd5395caaa5a16362b639
simao <gabrielsimao@hotmail.com>
2020-08-26 21:57:25 -0300

    Visualização dos Alunos existentes - 2

--- src/main/java/com/conquer/cursos/controller/AlunoController.java
+++ src/main/java/com/conquer/cursos/controller/AlunoController.java
@@ -3,30 +3,58 @@ package com.conquer.cursos.controller;
 import com.conquer.cursos.DTO.AlunoDTO;
 import com.conquer.cursos.DTO.AlunoNewDTO;
 import com.conquer.cursos.model.entity.Aluno;
+import com.conquer.cursos.model.entity.Turma;
 import com.conquer.cursos.service.AlunoService;
+import com.conquer.cursos.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/alunos")
 public class AlunoController {
-
 
     @Autowired
     private AlunoService service;
 
-    @PostMapping
-    public ResponseEntity<Void> insert(@Valid @RequestBody AlunoNewDTO dto) {
-        Aluno obj = service.fromDTO(dto);
-        obj = service.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).build();
+    @Autowired
+    private TurmaService turmaService;
+
+    @GetMapping
+    public String listarAlunos(Model model){
+        model.addAttribute("alunos", service.getAll());
+        return "alunos";
     }
+    @GetMapping("/novoAluno")
+    public String novoAluno(Model model) {
+        model.addAttribute("aluno", new AlunoNewDTO());
+        return "novoAluno";
+    }
+
+    @PostMapping
+    public String insertAluno(@Valid @ModelAttribute AlunoNewDTO dto, RedirectAttributes redirectAttributes){
+        Aluno aluno = service.fromDTO(dto);
+        aluno = service.insert(aluno);
+        redirectAttributes.addFlashAttribute("aluno", aluno);
+        return "redirect:/matricular";
+    }
+
+//
+//    @PostMapping
+//    public ResponseEntity<Void> insert(@Valid @RequestBody AlunoNewDTO dto) {
+//        Aluno obj = service.fromDTO(dto);
+//        obj = service.insert(obj);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+//        return ResponseEntity.created(uri).build();
+//    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Aluno> find(@PathVariable Integer id){
